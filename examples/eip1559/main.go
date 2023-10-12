581a52d0dcf170a17fc454675a5a81382c092cf9
DataWallet <dengchongjian@qq.com>
2023-10-13 02:19:22 +0800

    Update main.go

--- examples/eip1559/main.go
+++ examples/eip1559/main.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/chenzhijie/go-web3"
-	"github.com/chenzhijie/go-web3/utils"
+	"github.com/svija/go-web3"
+	"github.com/svija/go-web3/utils"
 )
 
 func main() {
