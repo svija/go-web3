5a6fa53061b4da4eadffa139c91a0b36d61d63da
DataWallet <dengchongjian@qq.com>
2023-10-13 02:23:08 +0800

    Update erc20.go

--- apps/erc20/erc20.go
+++ apps/erc20/erc20.go
@@ -7,9 +7,9 @@ import (
 	"sync/atomic"
 	"time"
 
-	"github.com/chenzhijie/go-web3"
-	"github.com/chenzhijie/go-web3/eth"
-	"github.com/chenzhijie/go-web3/types"
+	"github.com/svija/go-web3"
+	"github.com/svija/go-web3/eth"
+	"github.com/svija/go-web3/types"
 	"github.com/ethereum/go-ethereum/common"
 	eTypes "github.com/ethereum/go-ethereum/core/types"
 )
