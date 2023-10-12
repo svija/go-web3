254b4835b14614bd3b680c0a080131369a28b31d
DataWallet <dengchongjian@qq.com>
2023-10-13 02:19:34 +0800

    Update erc20.go

--- examples/contract/erc20.go
+++ examples/contract/erc20.go
@@ -5,8 +5,8 @@ import (
 	"math/big"
 	"os"
 
-	"github.com/chenzhijie/go-web3"
-	"github.com/chenzhijie/go-web3/types"
+	"github.com/svija/go-web3"
+	"github.com/svija/go-web3/types"
 	"github.com/ethereum/go-ethereum/common"
 )
 
