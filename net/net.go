0c3da7ba47908b0f4c488d6a9b987e38537009f8
DataWallet <dengchongjian@qq.com>
2023-10-13 02:18:34 +0800

    Update net.go

--- net/net.go
+++ net/net.go
@@ -1,8 +1,8 @@
 package net
 
 import (
-	"github.com/chenzhijie/go-web3/rpc"
-	"github.com/chenzhijie/go-web3/utils"
+	"github.com/svija/go-web3/rpc"
+	"github.com/svija/go-web3/utils"
 )
 
 type Net struct {
